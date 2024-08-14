e47fbfc6cce97baa23b341a8f90e3686f1c4a8c3
Leejoongjae123 <ljj3347@naver.com>
2024-08-14 12:46:40 +0900

    240814

---
+++ app/notification/components/NotificationEvent.js
@@ -0,0 +1,116 @@
+"use client";
+import { useState, useEffect } from "react";
+import { createClient } from "@/utils/supabase/client";
+
+function NotificationEvent() {
+  const [activeTab, setActiveTab] = useState(0);
+  const supabase = createClient();
+  const [events, setEvents] = useState([]);
+  const [tabName, setTabName] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      let tableName = "";
+      if (activeTab === 0) {
+        tableName = "notification";
+        setTabName("notification");
+      } else if (activeTab === 1) {
+        tableName = "promotion";
+        setTabName("promotion");
+      } else if (activeTab === 2) {
+        tableName = "manager";
+        setTabName("manager");
+      }
+
+      const { data, error } = await supabase
+        .from(tableName)
+        .select("*")
+        .limit(8);
+
+      if (error) {
+        console.error("Error fetching data:", error);
+      } else {
+        setEvents(data);
+      }
+    };
+
+    fetchData();
+  }, [activeTab]);
+  console.log(events);
+
+  return (
+    <>
+      <div className="page-heading">
+        <h1 className="page-title"> 이벤트 </h1>
+        <nav className="nav__underline">
+          <ul
+            uk-tab
+            className="group"
+            uk-switcher="connect: #ttabs ; animation: uk-animation-slide-right-medium, uk-animation-slide-left-medium"
+          >
+            <li>
+              {" "}
+              <a onClick={() => setActiveTab(0)}> 공지사항 </a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a onClick={() => setActiveTab(1)}> 프로모션 이벤트 </a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a onClick={() => setActiveTab(2)}> 매니저 이야기 </a>{" "}
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="relative mt-5" tabindex="-1" uk-slider="finite:true">
+        <div className="uk-slider-container pb-1">
+          <ul className="uk-slider-items grid-small">
+            {events.map((event, index) => (
+              <li key={index} className="lg:w-1/4 sm:w-1/3 w-1/2">
+                <div className="card h-full">
+                  <a href={`notification/${tabName}/${event.id}`}>
+                    <div className="card-media h-32">
+                      <img src={event.thumbImage} alt="" />
+                      <div className="card-overly"></div>
+                    </div>
+                  </a>
+                  <div className="card-body flex flex-col">
+                    <div>
+                      <p className="text-xs font-medium text-blue-600 mb-1">
+                        {event.title}
+                      </p>
+                      <a href={`notification/${tabName}/${event.id}`}>
+                        <h4 className="card-title text-sm">
+                          {event.description}
+                        </h4>
+                      </a>
+                      
+                    </div>
+                    <div className="flex flex-col items-end  card-list-info text-xs mt-1">
+                      <p className="card-text text-black">
+                        {event.creator}{" "}
+                      </p>
+                      <p> {event.regiDate} </p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <a className="nav-prev !top-20" href="#" uk-slider-item="previous">
+          {" "}
+          <ion-icon name="chevron-back" className="text-2xl"></ion-icon>{" "}
+        </a>
+        <a className="nav-next !top-20" href="#" uk-slider-item="next">
+          {" "}
+          <ion-icon name="chevron-forward" className="text-2xl"></ion-icon>
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default NotificationEvent;
---
+++ app/notification/components/NotificationIntroduction.js
@@ -0,0 +1,96 @@
+'use client'
+import {useState, useEffect} from 'react'
+import {createClient} from '@/utils/supabase/client'
+
+function NotificationIntroduction() {
+  const supabase = createClient()
+  const [postings, setPostings] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase
+        .from('introduction')
+        .select('*');
+
+      if (error) {
+        console.error('Error fetching data:', error);
+      } else {
+        setPostings(data);
+      }
+    };
+
+    fetchData();
+  }, []);
+  console.log(postings)
+  return (
+    <div
+      className="relative my-3"
+      tabindex="-1"
+      uk-slider="autoplay: true;finite: false;autoplayInterval:2000"
+    >
+      <div className="uk-slider-container pb-1">
+        <ul
+          className="uk-slider-items w-[calc(100%+14px)]"
+          uk-scrollspy="target: > li; cls: uk-animation-scale-up; delay: 20;repeat:true"
+        >
+          {postings.map((posting) => (
+            <li
+              className="pr-3 w-full"
+            uk-scrollspy-className="uk-animation-fade"
+          >
+            <div className="card">
+              <a href={`/notification/introduction/${posting.id}`}>
+                <div className="card-media sm:aspect-[2/1.7] h-72">
+                  <img src={posting.thumbImage} alt="" />
+                  <div className="card-overly"></div>
+                </div>
+              </a>
+              <div className="card-body relative">
+                <a href={`/notification/introduction/${posting.id}`}>
+                  <span className="text-teal-600 font-semibold text-xs">
+                    {posting.title}
+                  </span>
+                </a>
+                <a href={`/notification/introduction/${posting.id}`}>
+                  <p className="card-text block text-black mt-0.5">{posting.description} </p>
+                </a>
+
+              </div>
+            </div>
+          </li>
+          ))}
+          
+          
+          
+        </ul>
+      </div>
+
+      <div className="max-md:hidden">
+        <a
+          className="nav-prev !bottom-1/2 !top-auto"
+          href="#"
+          uk-slider-item="previous"
+        >
+          {" "}
+          <ion-icon name="chevron-back" className="text-2xl"></ion-icon>{" "}
+        </a>
+        <a
+          className="nav-next !bottom-1/2 !top-auto"
+          href="#"
+          uk-slider-item="next"
+        >
+          {" "}
+          <ion-icon name="chevron-forward" className="text-2xl"></ion-icon>
+        </a>
+      </div>
+
+      <div className="flex justify-center">
+        <ul className="inline-flex flex-wrap justify-center my-5 gap-2 uk-dotnav uk-slider-nav">
+          {" "}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default NotificationIntroduction;

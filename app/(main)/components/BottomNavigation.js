ca3eef81a9c322ba784309afbcb82a366cb731ff
Leejoongjae123 <ljj3347@naver.com>
2024-08-28 17:36:20 +0900

    240828

--- app/(main)/components/BottomNavigation.js
+++ app/(main)/components/BottomNavigation.js
@@ -9,7 +9,7 @@ export default function BottomNavigationBar() {
   const router = useRouter();
 
   return (
-    <div class="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600 shadow-md">
+    <div class="fixed z-50 w-[80vw] h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-2 left-1/2 dark:bg-gray-700 dark:border-gray-600 shadow-md">
       <div class="grid h-full max-w-lg grid-cols-3 mx-auto">
         <button
           data-tooltip-target="tooltip-home"

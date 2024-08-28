924fedd7682906431df6db1718c9dc5610e43490
Leejoongjae123 <ljj3347@naver.com>
2024-08-28 17:36:41 +0900

    240828

--- app/(main)/components/BottomNavigation.js
+++ app/(main)/components/BottomNavigation.js
@@ -42,6 +42,7 @@ export default function BottomNavigationBar() {
             data-tooltip-target="tooltip-new"
             type="button"
             class="inline-flex items-center justify-center w-10 h-10 font-medium bg-primary rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
+            onClick={() => router.push("/write")}
           >
             <svg
               class="w-4 h-4 text-white"

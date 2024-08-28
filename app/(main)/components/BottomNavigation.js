a659df2d15e574b93c249271b4f097f57e809ea1
Leejoongjae123 <ljj3347@naver.com>
2024-08-28 17:38:49 +0900

    240828

--- app/(main)/components/BottomNavigation.js
+++ app/(main)/components/BottomNavigation.js
@@ -9,16 +9,16 @@ export default function BottomNavigationBar() {
   const router = useRouter();
 
   return (
-    <div class="fixed z-50 w-[80vw] h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-2 left-1/2 dark:bg-gray-700 dark:border-gray-600 shadow-md">
-      <div class="grid h-full max-w-lg grid-cols-3 mx-auto">
+    <div className="fixed z-50 w-[90vw] h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-2 left-1/2 dark:bg-gray-700 dark:border-gray-600 shadow-md md:hidden">
+      <div className="grid h-full max-w-lg grid-cols-3 mx-auto">
         <button
           data-tooltip-target="tooltip-home"
           type="button"
           onClick={() => router.push("/")}
-          class="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          className="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
         >
           <svg
-            class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+            className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -26,26 +26,26 @@ export default function BottomNavigationBar() {
           >
             <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
           </svg>
-          <span class="sr-only">Home</span>
+          <span className="sr-only">Home</span>
         </button>
         <div
           id="tooltip-home"
           role="tooltip"
-          class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
         >
           Home
-          <div class="tooltip-arrow" data-popper-arrow></div>
+          <div className="tooltip-arrow" data-popper-arrow></div>
         </div>
 
-        <div class="flex items-center justify-center pp5">
+        <div className="flex items-center justify-center pp5">
           <button
             data-tooltip-target="tooltip-new"
             type="button"
-            class="inline-flex items-center justify-center w-10 h-10 font-medium bg-primary rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
+            className="inline-flex items-center justify-center w-10 h-10 font-medium bg-primary rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
             onClick={() => router.push("/write")}
           >
             <svg
-              class="w-4 h-4 text-white"
+              className="w-4 h-4 text-white"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -59,26 +59,26 @@ export default function BottomNavigationBar() {
                 d="M9 1v16M1 9h16"
               />
             </svg>
-            <span class="sr-only">New item</span>
+            <span className="sr-only">New item</span>
           </button>
         </div>
         <div
           id="tooltip-new"
           role="tooltip"
-          class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
         >
           Create new item
-          <div class="tooltip-arrow" data-popper-arrow></div>
+          <div className="tooltip-arrow" data-popper-arrow></div>
         </div>
 
         <button
           data-tooltip-target="tooltip-profile"
           type="button"
-          class="inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          className="inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
           onClick={() => router.push("/mypage")}
         >
           <svg
-            class="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+            className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -86,15 +86,15 @@ export default function BottomNavigationBar() {
           >
             <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
           </svg>
-          <span class="sr-only">Profile</span>
+          <span className="sr-only">Profile</span>
         </button>
         <div
           id="tooltip-profile"
           role="tooltip"
-          class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
         >
           Profile
-          <div class="tooltip-arrow" data-popper-arrow></div>
+          <div className="tooltip-arrow" data-popper-arrow></div>
         </div>
       </div>
     </div>
